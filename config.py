b55f7c645e07b640107060d72997115a59e490b0
Grace Jia <gracejia25@gmail.com>
2026-04-20 15:59:25 -0400

    Add SWAT defense

--- config.py
+++ config.py
@@ -1,3 +1,25 @@
 RAW_DATASET_FOLDER = 'datasets_raw'
 PRE_DATASET_FOLDER = 'datasets_pre'
-PRO_DATASET_FOLDER = 'datasets_pro'
+PRO_DATASET_FOLDER = '../datasets_pro'
+
+# debug.py
+NKW = 250
+NQR = 1_000_000
+NITERS = 10_000
+NITER_LIST = [0, 100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+PFREE = 0.25
+
+NUM_TARGETS_PAIRS = 50  # number of target kws for pairs attack
+THETA = 1               # can't sample from pool until pool size > THETA (following initial SWAT implementation in decorr.py)
+SAMPLING_FUNC = "Exp"   # sampling pool strategy (None, "Linear", "Exp")
+
+NRUNS = 5
+
+# experiment.py
+CORR_LEVEL = 'mid'  # 'high': each kw only transitions to one (first/random) doc containing it - see HIGH_CORR_PERMUTE
+                    # 'mid' : each kw can transition to any doc containing it, but weighted exponentially
+                    # 'low' : each kw can transition to any doc containing it (weighted equally)
+HIGH_CORR_PERMUTE = True # If CORR_LEVEL = 'high' and HIGH_CORR_PERMUTE = True, each kw only transitions to random doc containing it
+                          # If False, each kw only transitions to first doc containing it
+
+BASE_SEED = 58 # debug.py runs deterministically; introduce randomness in case a run gets interrupted and we want to restart without redoing all of the previously-done randomness
--- defense.py
+++ defense.py
@@ -1,7 +1,5 @@
 import numpy as np
 import utils
-import itertools
-from time import time
 from collections import defaultdict
 
 
@@ -74,41 +72,53 @@ def generate_observations(full_data_client, def_params, real_queries):
         observations['n_distinct'] = len(set(real_queries))
         real_and_dummy_queries = real_queries
 
-    elif def_params['name'] == 'pancake':
+    if def_params['name'] == 'pancake' or def_params['name'] == 'swat':
 
         trace_type = 'tok_vol'
         freal = utils.get_steady_state(full_data_client['frequencies']) if full_data_client['frequencies'].ndim == 2 else full_data_client['frequencies']
         prob_reals, prob_dummies, replicas_per_kw = utils.compute_pancake_parameters(nkw, freal)
+
+        # some imprecision in calculating prob_reals above causes some values to be very small negative numbers,
+        # which then was causing errors later in the attack when we expect probabilities to be nonnegative
+        prob_reals[prob_reals<0] = -prob_reals[prob_reals<0]
+
         permutation = np.random.permutation(2 * nkw)
         aux = [0] + list(np.cumsum(replicas_per_kw, dtype=int))
-        kw_id_to_replica = [tuple(permutation[aux[i]: aux[i + 1]]) for i in range(len(aux) - 1)]
-
-        nq = len(real_queries)
-        perm = np.random.permutation(3 * nq)
-        separation = np.random.binomial(3 * nq, 0.5)
-        indices_real_slots, indices_dummy_slots = perm[:separation], perm[separation:]
-        indices_real_slots.sort()
-        indices_for_each_true_message = []
-        real_slots_copy = indices_real_slots.copy()
-        for i in range(0, 3 * nq, 3):
-            try:
-                index = next(filter(lambda x: real_slots_copy[x] >= i, range(len(real_slots_copy))))
-            except StopIteration:
-                break
-            indices_for_each_true_message.append(real_slots_copy[index])
-            real_slots_copy = real_slots_copy[(index + 1):]
-
-        trace_no_replicas = -np.ones(3 * nq, dtype=int)
-        trace_no_replicas[indices_dummy_slots] = np.random.choice(nkw + 1, len(indices_dummy_slots), replace=True, p=prob_dummies)
-        trace_no_replicas[indices_real_slots] = np.random.choice(nkw + 1, len(indices_real_slots), replace=True, p=prob_reals)
-        trace_no_replicas[indices_for_each_true_message] = real_queries[:len(indices_for_each_true_message)]
-
-        for kw_id in trace_no_replicas:
-            traces.append((np.random.choice(kw_id_to_replica[kw_id]), 1))  # Volume is 1
-
-        real_and_dummy_queries = trace_no_replicas
+        kw_id_to_replica = [tuple(permutation[aux[i]: aux[i + 1]]) for i in range(len(aux) - 1)] # kw_id_to_replica[id] is a tuple w all replicas matching kw id
 
         bw_overhead = 3
+
+        if def_params['name'] == 'swat': # add SWAT sampling pool
+            traces, real_and_dummy_queries = generate_theta_decorr_obs(nkw, kw_id_to_replica, real_queries, prob_reals, prob_dummies)
+        else: # just pancake
+            nq = len(real_queries)
+            perm = np.random.permutation(3 * nq)
+            separation = np.random.binomial(3 * nq, 0.5)
+            indices_real_slots, indices_dummy_slots = perm[:separation], perm[separation:]
+            indices_real_slots.sort()
+            indices_for_each_true_message = []
+            real_slots_copy = indices_real_slots.copy()
+            for i in range(0, 3 * nq, 3):   # maximum number of real slots chosen before reaching query i in nq is i/3 slots;
+                                            # models injecting a new query from real_queries once every 3 requests
+                try:
+                    index = next(filter(lambda x: real_slots_copy[x] >= i, range(len(real_slots_copy))))
+                except StopIteration:
+                    break
+                indices_for_each_true_message.append(real_slots_copy[index])
+                real_slots_copy = real_slots_copy[(index + 1):]
+
+            trace_no_replicas = -np.ones(3 * nq, dtype=int)
+            trace_no_replicas[indices_dummy_slots] = np.random.choice(nkw + 1, len(indices_dummy_slots), replace=True, p=prob_dummies)
+            trace_no_replicas[indices_real_slots] = np.random.choice(nkw + 1, len(indices_real_slots), replace=True, p=prob_reals)
+            trace_no_replicas[indices_for_each_true_message] = real_queries[:len(indices_for_each_true_message)]
+
+            for kw_id in trace_no_replicas:
+                traces.append((np.random.choice(kw_id_to_replica[kw_id]), 1))  # Volume is 1
+
+            real_and_dummy_queries = trace_no_replicas
+
+    elif def_params['name'] == 'waffle':
+        raise NotImplementedError("Waffle defense not yet implemented")
 
     else:
         raise ValueError("Defense {:s} not implemented".format(def_params['name']))
@@ -117,4 +127,96 @@ def generate_observations(full_data_client, def_params, real_queries):
     observations['trace_type'] = trace_type
     observations['ndocs'] = len(dataset)
 
-    return observations, bw_overhead, real_and_dummy_queries
+    correct_mapping = {int(replica): kw_id for kw_id, replicas in enumerate(kw_id_to_replica) for replica in replicas}
+    return observations, bw_overhead, real_and_dummy_queries, correct_mapping
+
+
+def generate_theta_decorr_obs(nkw, kw_id_to_replica, real_queries, prob_reals, prob_dummies):
+    # SWAT query pool implementation based on original codebase: https://github.com/CongGroup/SWAT/blob/main/query_decorrelation.py
+    from config import NQR, THETA, SAMPLING_FUNC
+
+    que = SamplingPool(THETA, SAMPLING_FUNC)
+    q_szs = {} # for debug
+    cnt = 0 # counter; essentially a timestamp
+
+    real_and_dummy_queries = []
+    detailedTranscript = []
+
+    traces = []
+
+    print("nkw:", nkw)
+    print("real_queries[:25]", real_queries[:25])
+
+    for q in real_queries:
+        if (len(real_and_dummy_queries) >= NQR): continue
+
+        # for debug, delete
+        if (len(real_and_dummy_queries)%100_000 == 0): print(len(real_and_dummy_queries), "observations generated")
+
+        que.put((q, cnt))
+        latencies = []
+        for i in range(3):
+            delta = 0.5
+            type = np.random.choice([0, 1], p=[delta, 1-delta])
+            if type == 0:
+                idx = np.random.choice(nkw+1, p=prob_dummies)
+            else:
+                if que.qsize() > THETA:
+                    idx, cnt_ = que.get()
+                    latencies.append(cnt - cnt_)
+                    type = 0 # not a real query from transcript
+                else:
+                    idx = np.random.choice(nkw+1, p=prob_reals)
+
+            key_ = np.random.choice(kw_id_to_replica[idx])
+
+            real_and_dummy_queries.append(idx)
+            traces.append((key_, 1))
+
+            # for debug
+            detailedTranscript.append( (idx, key_, type == True) )
+            if que.qsize() not in q_szs: q_szs[que.qsize()] = 0
+            q_szs[que.qsize()] += 1
+
+        cnt+=1
+
+    print("First 100 queries (key, replica, isRealQueryFromTranscript):", detailedTranscript[:100])
+
+    return traces, np.array(real_and_dummy_queries)
+
+class SamplingPool:
+    def __init__(self, initNum = 0, update = None):
+        self.pool = []
+        self.initNum = initNum
+        self.update = update
+        self.weight = []
+        self.q_szs = {}
+
+    def get(self):
+        ret = 0
+        if self.update is None:
+            t = np.random.randint(0, len(self.pool))
+            ret = self.pool.pop(t)
+        elif self.update == "Linear":
+            t = np.random.choice(np.arange(len(self.pool)),
+                                p=np.array(self.weight) / np.sum(self.weight))
+            ret = self.pool.pop(t)
+            self.weight.pop(t)
+            self.weight = [x + 3 for x in self.weight]
+        elif self.update == "Exp":
+            t = np.random.choice(np.arange(len(self.pool)),
+                                p=np.array(self.weight) / np.sum(self.weight))
+            ret = self.pool.pop(t)
+            self.weight.pop(t)
+            self.weight = [x * 5 for x in self.weight]
+        return ret
+
+    def qsize(self):
+        return len(self.pool)
+
+    def empty(self) -> bool:
+        return len(self.pool) <= self.initNum
+
+    def put(self, x):
+        self.pool.append(x)
+        self.weight.append(1)
